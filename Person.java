b69d7d5fb85234b956ff508a85b2325f0f59e4e9
ILMA MALIK <26ilmamalik@gmail.com>
2023-08-28 11:39:36 +0530

    Person.java

---
+++ Person.java
@@ -0,0 +1,7 @@
+//Q.2 Create an interface called "Person" with a method called "speak".Create two classes called "Student" and "Teacher" that implement the Person interface and implement the "speak" method. Create objects of both the Student and  Teacher classes and call their respective "speak"  methods.
+//3 Files: Person.java, Teacher.java, Student.java
+package Thursdaylab_3Aug;
+public interface Person 
+{
+	void speak();				//Abstract Function called speak
+}
